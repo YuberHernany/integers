de878ba4ef92df9c5551fc6bd5dede98870e8142
Yuber Hernany <yuber.tapias@udea.edu.co>
2024-12-11 19:36:18 -0500

    adding image

--- code_with_sound.py
+++ code_with_sound.py
@@ -13,6 +13,14 @@ class FadeInSquare(Scene):
 '''
         rendered_code = Code(code=code, tab_width=4, background="window",
                             language="Python", font="Monospace")
+        rendered_code.to_edge(LEFT)
+
+        # now image
+        typing_image = ImageMobject("assets/images/typing.jpeg").to_edge(DR, buff=0)
+        rec_typing = SurroundingRectangle(typing_image, stroke_width=30).set_color(RED)
+        self.play(FadeIn(typing_image), FadeIn(rec_typing))
+        self.wait(1)
+
         self.play(FadeIn(rendered_code.background_mobject))
         self.wait(1)
         self.play(FadeIn(rendered_code.line_numbers))
@@ -24,5 +32,6 @@ class FadeInSquare(Scene):
                 self.play(AddTextLetterByLetter(line), time_per_char=0.01)
         self.play(AddTextLetterByLetter(rendered_code.code[2:]))
         self.wait(2)
+
 
 # ensuring that the file with an example of code is on the project
